a97d21f9a2f90b6e419f800beb8415468020ecdc
Ben <robinsonbena@gmail.com>
2022-03-05 18:13:02 -0800

    solution using each_with_index

--- README.md
+++ README.md
@@ -1,15 +1,35 @@
-This stock picker is built in Ruby for The Odin Project. There is an array of prices for a stock, where each index is a day with a different stock price. This algorithm determines which days to buy and sell to maximise gains, taking into account cases when e.g., the highest stock price comes first, or lowest price comes last. You can download picker.rb and view locally if you have Ruby insalled, or run online at: 
+This stock picker is built in Ruby for The Odin Project. There is an array of prices for a stock, where each element is a day with a different stock price. This algorithm determines which days to buy and sell to maximise gains, taking into account cases when e.g., the highest stock price comes first, or lowest price comes last. You can download picker.rb and view locally if you have Ruby insalled, or you can run online below. 
+
+Notes: I first solved this without the each_with_index enumerable, and took a pretty complex route. I then checked other solutions and saw the each_with_index enumerable is tailor made for this problem, and took just 10 minutes to write after...an important lesson to know the Ruby enumerables and what they can do!
+
+First solution without each_with_index: https://replit.com/@BenJ8/stock-picker#main.rb
+Second solution with each_with_index: https://replit.com/@BenJ8/stock-picker-each-with-index#main.rb 
 
 
-<!-- prices.each do |i|
-    hash = {}
+<!-- stock_prices = [17,3,6,9,15,8,6,1,10]
+
+def picker(prices)
+
+  #make hash with unique keys that equal the indexes of prices array
+  #cannot use prices.each because duplitcate prices are stored in same key, not uniquely
+  hash = {}
+  prices.each_index {|i| hash[i] = []}
+  puts hash
+
+  #iterate over stock_prices to take one value and subtract each successive value
+  #from it, and store each result as an element in hash
+  #need to make an array of arrays to handle duplicate numbers!!!!!!!!!
+  prices.each do |i|
     c = 1
     while c <= prices.length - prices.index(i) -1 do
-      hash[i] = [i - prices[prices.index(i) + c]]
+      hash[prices.index(i)] << i - prices[prices.index(i) + c]
       c += 1
-    end
-    puts hash      
+    end     
   end
+  puts hash
 end
 
 picker(stock_prices) -->
+
+
+
--- picker.rb
+++ picker.rb
@@ -1,45 +1,20 @@
 def picker(prices)
-
-  #make a hash with key (day) and value (price)
-  #needs to be hash because duplicate values in an array prevent code below working properly
-  prices_hash = {}
-  prices.each_index {|i| prices_hash[i] = prices[i]}
-  
-  #make an array of prices_hash keys to so you can reference hash index on line 21
-  prices_hash_keys = prices_hash.keys
-
-  #make second hash with key (day) and value [] empty array for use below
-  #cannot use prices.each because duplicate prices get stored in same key, not uniquely like index
-  output_hash = {}
-  prices.each_index {|i| output_hash[i] = []}
-
-  #loop through prices_hash, and for each k,v pair, execute a while loop that iterates 
-  #euqal to hash length minus the position of the given key (key index pulled from line 9)
-  #then subtract the subsequent values in the hash from v, and store output in output_hash
-  prices_hash.each do |k, v|
-    c = 1
-    while c <= prices_hash.length - prices_hash_keys.index(k) -1 do
-      output_hash[k] << v - prices_hash[k + c]
-      c += 1
-    end     
+  buy = 0
+  sell = 0
+  profit = 0  
+  prices.each_with_index do |buy_price, buy_day|
+    prices.each_with_index do |sell_price, sell_day|
+      if sell_day > buy_day
+        if sell_price - buy_price > profit
+          profit = sell_price - buy_price
+          buy = buy_day
+          sell = sell_day
+        end
+      end
+    end
   end
-  
-  #loop through output_hash to find lowest value in each key and push to lowest_array
-  #values are (buy - sell) so a negative value is a gain and positive is a loss
-  lowest_array = []
-  output_hash.each do |k,v|
-    lowest_array << v.min
-  end
-
-  #get rid of any nil values if they exist
-  #buy day is the hash index where the lowest number out of all values exists
-  #sell day is buy day index + index of lowest number in its array, + 1
-  lowest_array.compact!
-  buy_day = lowest_array.index(lowest_array.min)
-  sell_day = buy_day + output_hash[buy_day].index(output_hash[buy_day].min) + 1
-  puts "buy on day #{buy_day}"
-  puts "sell on day #{sell_day}"
-  
+  puts "buy on day #{buy} and sell on day #{sell}"
+  puts "your profit will be $#{profit} ka-ching ka-ching"
 end
 
 picker([17,3,6,9,8,15,6,1,10])
